2866557966be37b45a79b13622f4263ff3cdf472
Furkan <furkan6153@gmail.com>
2026-02-01 20:49:18 +0300

    build for github pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import svgr from "vite-plugin-svgr";
 export default defineConfig({
   base: "/Product-list-with-cart/",
   plugins: [react(), svgr()],
+  build: {
+    outDir: ".",    
+    emptyOutDir: false
+  }
 });
