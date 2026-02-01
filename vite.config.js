4ed26095d448612a5ee2cc6a9aca60ef68ec6214
Furkan <furkan6153@gmail.com>
2026-02-01 21:49:34 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-  base: "/Product-list-with-cart/",
   plugins: [react(), svgr()],
-  build: {
-    outDir: ".",    
-    emptyOutDir: false
-  }
 });
